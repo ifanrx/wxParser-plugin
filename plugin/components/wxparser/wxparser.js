60482b54936ca4084de077228953a8d1a1c96b29
xiejiayun <xiejiayun@ifanr.com>
2020-03-02 15:43:01 +0800

    issue 更新

--- plugin/components/wxparser/wxparser.js
+++ plugin/components/wxparser/wxparser.js
@@ -6,15 +6,47 @@ Component({
     richText: {
       type: String,
       value: '',
-      observer: function (richText) {
-        let parsedData = parse(richText)
-        this.setData({ richTextNode: parsedData.nodes })
-        tmpl.init.call(this, { imgUrls: parsedData.imageUrls })
-      }
+    },
+    imageLazyLoad: {
+      type: Boolean,
+      value: false,
+    },
+    imageZoom: {
+      type: Boolean,
+      value: true,
+    },
+  },
+
+  observers: {
+    'richText, imageLazyLoad': function(richText, imageLazyLoad) {
+      let parsedData = parse(richText)
+      this.setData({ richTextNode: parsedData.nodes.map(item => {
+        item.imageLazyLoad = imageLazyLoad
+        return item
+      })})
+      tmpl.init.call(this, { imgUrls: parsedData.imageUrls })
     }
+  },
+
+  options: {
+    styleIsolation: 'apply-shared',
   },
 
   data: {
     richTextNode: ''
+  },
+
+  lifetimes: {
+    attached: function () {
+      this.triggerEvent('attached')
+    },
+
+    ready: function() {
+      this.triggerEvent('ready')
+    },
+
+    detached: function() {
+      this.triggerEvent('detached')
+    }
   }
 })
